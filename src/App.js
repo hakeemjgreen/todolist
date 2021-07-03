8da8ee4ab2b6953e30ec78f6159dadcfe5fe48b0
Hakeem Codes <hakeemcodes@MacBook-Pro.local>
2021-07-03 02:14:04 -0400

    Added CSS animations

--- src/App.js
+++ src/App.js
@@ -47,27 +47,17 @@ function App() {
         <div className="w-100-l mh3 inline-flex items-center">
           {/* Set input text to state, if empty set to empty string */}
           <input id="note-bar" className="w-100 w-70-ns pa3 effect-1" type="text" value={todo.content ? todo.content : ''} onChange={(e)=>{onTextChange(e.target.value)}} />
-          {/* <a id="submit-btn" href="#" className="w-100 w-20-ns f5 effect-1 no-underline black bg-animate inline-flex items-center pa3 ba border-box ml2" onClick={
-            ()=>{onSubmit()
-              successHandler("Added")}
-
-            }>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
-            </svg>
-            <span className="pl1">{noteSuccess}</span>
-            
-          </a>  */}
-          <input id="submit-btn" disabled={!todo.content ? true : false} className="w-100 w-20-ns f5 effect-1 no-underline black bg-animate inline-flex items-center pa3 ba border-box ml2" type="submit"  value={noteSuccess} onClick={
-            ()=>{onSubmit()
+          <input id="submit-btn" disabled={!todo.content ? true : false} className={`${noteSuccess === "Added" ? 'animate__animated animate__pulse white bg-green' : ''} w-100 w-20-ns f5 effect-1 no-underline black bg-animate inline-flex items-center pa3 ba border-box ml2`} type="submit"  value={noteSuccess} onClick={
+            ()=>{
+              onSubmit()
               successHandler("Added")}
 
             } />      
         </div>
         
-        <ul class="list ph0 mt0 f4 tl w-100">
+        <ul className="list ph0 mt0 f4 tl w-100">
           {todoArray.map((item, i)=>(
-            <li class="lh-copy pv3 ph4 ba bl-0 bt-0 br-0 b--black-30 w-100" key={i}>{item.content}
+            <li className="animate__animated animate__fadeInLeft lh-copy pv3 ph4 ba bl-0 bt-0 br-0 b--black-30 w-100" key={i}>{item.content}
               <a disabled onClick={()=>{deleteTodo(item.id)}} href="#">Delete</a>
             </li>
           ))}
